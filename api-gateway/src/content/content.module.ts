a370f6d3940da57917055a3ba78f51143cb0a12d
Ibra <izuberi844@gmail.com>
2026-05-31 09:04:39 +0300

    feat: redis caching on content feed and item endpoints

--- api-gateway/src/content/content.module.ts
+++ api-gateway/src/content/content.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Content } from './entities/content.entity';
 import { ContentService } from './content.service';
+import { CacheService } from '../cache/cache.service'
 import { ContentController } from './content.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Content])],
-  providers: [ContentService],
+  providers: [ContentService, CacheService],
   controllers: [ContentController],
 })
 export class ContentModule {}
